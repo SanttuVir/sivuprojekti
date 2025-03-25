9c7c9682da681b2f86aef63d31fdc2e86549d95f
SanttuVir <santtupoika02@gmail.com>
2025-03-25 12:41:42 +0200

    admin sivun lisäystä
    Tekemässä admin pagea menun muokkaukseen ja varausten näkemiseen

---
+++ sivu/Tilaa/Admin.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" 
+    content="width=device-width, 
+    initial-scale=1.0">
+    <title>webbisivu</title>
+    <link rel="stylesheet" href="/sivu/navigointi.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Playwrite+VN:wght@100..400&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Jersey+15&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+    <div id="main__container"></div>
+    <nav class="navbar">
+        <div class="navbar__container">
+            <a href="/" id="navbar__logo">Ruokaa</a>
+            <div class="navbar__toggle" id="mobile-menu">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+            </div>
+            <ul class="navbar__menu">
+                <li class="navbar__item">
+                    <a href="/sivu/Etusivu/index.html" class="navbar__links"> 
+                        Kotisivu
+                    </a>
+                </li>
+                <li class="navbar__item">
+                    <a href="Menu.html" class="navbar__links"> 
+                        Menu 
+                    </a>
+                </li>
+                <li class="navbar__item">
+                    <a href="Buffetti.html" class="navbar__links"> 
+                        Buffetti
+                    </a>
+
+                    <li class="navbar__item">
+                        <a href="Tilaa.html" class="navbar__links"> 
+                            Tilaa                        
+                        </a>
+                </li>
+                <li class="navbar__btn">
+                    <a href="Palaute.html" class="Button"> 
+                        Palaute
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <script src="app.js"></script>
+</body>
+</html>
